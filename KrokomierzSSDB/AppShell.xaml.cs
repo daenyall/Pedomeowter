f3259a8727fef1f307c3dc946a16acf019ca8644
danie <danie@DESKTOP-L5J9A4Q>
2024-11-30 16:05:55 +0100

    Dodaj pliki projektów.

---
+++ KrokomierzSSDB/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace KrokomierzSSDB
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ KrokomierzSSDB/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace KrokomierzSSDB
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
