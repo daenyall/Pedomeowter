46a2c93c02bfa240a0ead3e77ee049e0d196f9bd
[Seweryn Słowik] <[sewerynslowik1@gmail.com]>
2024-12-07 13:29:31 +0100

    Added timer

--- KrokomierzSSDB/MainPage.xaml.cs
+++ KrokomierzSSDB/MainPage.xaml.cs
@@ -3,6 +3,9 @@
     public partial class MainPage : ContentPage
     {
         int count = 0;
+        DateTime timeStarted;
+        bool isTimerEnabled = false;
+
 
         public MainPage()
         {
@@ -12,6 +15,36 @@
         private void startToMeasure(object sender, EventArgs e)
         {
 
+            if (!isTimerEnabled)
+            {
+                timeStarted = DateTime.Now;
+                isTimerEnabled = true;
+                Thread thread = new Thread(timer);
+                thread.Start();
+            }
+            else
+            {
+                isTimerEnabled = false;
+            }
+
+        }
+
+        void timer() { 
+            if(!isTimerEnabled)
+            {
+                return;
+            }
+
+            DateTime time = DateTime.Now;
+            TimeSpan timePassed = time - timeStarted;
+            Dispatcher.Dispatch(new Action(() => {
+                timeLabel.Text = timePassed.ToString();
+            }
+                
+            )) ;
+
+            Thread.Sleep(10);
+            timer();
         }
     }
 
