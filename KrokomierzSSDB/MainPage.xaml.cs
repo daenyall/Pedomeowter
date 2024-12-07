ab58a1b0037cc501d3011ca077ae7051f24d2821
[Seweryn Słowik] <[sewerynslowik1@gmail.com]>
2024-12-07 14:23:22 +0100

    Added basic logic like timer, calculating distance (have to be checked), average speed and steps (variables with height should be able to be changed in the settings menu + pop-up should be added when launching the app for the first time)

--- KrokomierzSSDB/MainPage.xaml.cs
+++ KrokomierzSSDB/MainPage.xaml.cs
@@ -1,14 +1,26 @@
-﻿namespace KrokomierzSSDB
+﻿using CloudKit;
+
+namespace KrokomierzSSDB
 {
     public partial class MainPage : ContentPage
     {
         int count = 0;
         DateTime timeStarted;
         bool isTimerEnabled = false;
+        bool _isCheckingLocation;
+        CancellationTokenSource _cancelTokenSource;
+        Location oldLocation;
+        Location location;
+        int height = 180;
+        double stepLength;
+        double Distance = 0;
+        double steps;
+        double avgSpeed;
 
 
         public MainPage()
         {
+            stepLength = height * 0.36;
             InitializeComponent();
         }
 
@@ -29,6 +41,76 @@
 
         }
 
+
+        async Task getCurrentLocation()
+        {
+
+            if(location != null) { 
+            oldLocation = location;
+            try
+            {
+                _isCheckingLocation = true;
+
+                GeolocationRequest request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+
+                _cancelTokenSource = new CancellationTokenSource();
+
+                location = await Geolocation.Default.GetLocationAsync(request, _cancelTokenSource.Token);
+
+                if (location != null)
+                    Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+
+                }
+                catch (Exception ex)
+                {
+
+                }
+                finally
+                {
+                    _isCheckingLocation = false;
+                }
+            }
+            else
+            {
+                try
+                {
+                    _isCheckingLocation = true;
+
+                    GeolocationRequest request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+
+                    _cancelTokenSource = new CancellationTokenSource();
+
+                    location = await Geolocation.Default.GetLocationAsync(request, _cancelTokenSource.Token);
+                    oldLocation = location;
+
+                    if (location != null)
+                        Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+
+                }
+                catch (Exception ex)
+                {
+
+                }
+                finally
+                {
+                    _isCheckingLocation = false;
+                }
+            }
+
+        }
+
+        double calculateDistance() {
+            return Location.CalculateDistance(oldLocation, location, DistanceUnits.Kilometers);
+        }
+
+        double calculateSteps() {
+            return Distance / stepLength;
+        }
+
+        double calculateAvgSpeed(TimeSpan timePassed) {
+            return Distance / timePassed.TotalHours;
+        }
+
         void timer() { 
             if(!isTimerEnabled)
             {
@@ -39,11 +121,21 @@
             TimeSpan timePassed = time - timeStarted;
             Dispatcher.Dispatch(new Action(() => {
                 timeLabel.Text = timePassed.ToString();
+                getCurrentLocation();
+                Distance = calculateDistance();
+                distanceLabel.Text = Distance.ToString();
+                steps = calculateSteps();
+                stepsLabel.Text = steps.ToString();
+                avgSpeed = calculateAvgSpeed(timePassed);
+                averageSpeedLabel.Text = avgSpeed.ToString();
+                //Nie wiem na jakiej zasadzie chcesz liczyc kalorie dlatego tego juz nie dodaje okok
+                
             }
                 
             )) ;
 
-            Thread.Sleep(10);
+            Thread.Sleep(1000);
+            
             timer();
         }
     }
