a0cfa638c5417d60d6394dc6db53ad552614fd5c
[Seweryn Słowik] <sewerynslowik1@gmail.com>
2024-12-08 16:19:59 +0100

    Update of step counter logic, now based on Accelerometer not on geolocation (as it used to be)

--- KrokomierzSSDB/MainPage.xaml.cs
+++ KrokomierzSSDB/MainPage.xaml.cs
@@ -1,8 +1,37 @@
 ﻿
+using Microsoft.Maui;
+using static Microsoft.Maui.ApplicationModel.Permissions;
+using System.Diagnostics;
+
 namespace KrokomierzSSDB
 {
     public partial class MainPage : ContentPage
     {
+
+        private Stopwatch stopwatch = new Stopwatch();
+        private IDispatcherTimer timert;
+        private int stepsCount = 0;
+        private double distance = 0.0;
+        private double caloriesBurned = 0.0;
+        private double speed = 0.0;
+        private bool isTracking = false;
+        private bool isPaused = false; // Dodana flaga do �ledzenia pauzy
+        private IDispatcherTimer locationUpdateTimer;
+
+        private const double StepThreshold = 1.2;
+        private const int StepCooldown = 300;
+        private const double StepLength = 0.78;
+        private const double CaloriesPerMeter = 0.05;
+        private bool _isWaiting = false;
+
+        private DateTime _lastStepTime = DateTime.MinValue;
+        private DateTime _startTime = DateTime.MinValue;
+        private DateTime _lastUpdateTime = DateTime.MinValue;
+
+
+        //Moje zmienne ogolem nie oszusta
+
+
         int count = 0;
         DateTime timeStarted;
         bool isTimerEnabled = false;
@@ -17,125 +46,154 @@ namespace KrokomierzSSDB
         double avgSpeed;
 
 
+
+
+
+        
+
         public MainPage()
         {
             stepLength = height * 0.36;
             InitializeComponent();
+                // Inicjalizacja timera
+            timert = Dispatcher.CreateTimer();
+            timert.Interval = TimeSpan.FromMilliseconds(100);
+            timert.Tick += OnTimerTick;
+
+                RequestPermissions();
         }
 
         private void startToMeasure(object sender, EventArgs e)
         {
 
-            if (!isTimerEnabled)
+
+            if (stopwatch.IsRunning && !isPaused)
             {
-                timeStarted = DateTime.Now;
-                isTimerEnabled = true;
-                Thread thread = new Thread(timer);
-                thread.Start();
+                // Zatrzymanie stopera i zmiana statusu na pauzowany
+                stopwatch.Stop();
+                timert.Stop();
+                isPaused = true;
+                
+            }
+            else if (isPaused)
+            {
+                // Wznawianie liczenia po pauzie
+                stopwatch.Start();
+                timert.Start();
+                isPaused = false;
+                
             }
             else
             {
-                isTimerEnabled = false;
+                // Rozpocz�cie nowego treningu
+                stopwatch.Restart();
+                timert.Start();
+                
+                isTracking = true;
+
+                _startTime = DateTime.Now;
+                
+
+                
+                
             }
 
         }
 
-
-        async Task getCurrentLocation()
+        private void OnTimerTick(object sender, EventArgs e)
         {
-
-            if(location != null) { 
-            oldLocation = location;
-            try
+            if (!isPaused)
             {
-                _isCheckingLocation = true;
+                timeLabel.Text = stopwatch.Elapsed.ToString(@"hh\:mm\:ss");
+                UpdateDistance();
+                UpdateCalories();
+                UpdateAveragePace();
+            }
+        }
 
-                GeolocationRequest request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
 
-                _cancelTokenSource = new CancellationTokenSource();
 
-                location = await Geolocation.Default.GetLocationAsync(request, _cancelTokenSource.Token);
 
-                if (location != null)
-                    Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+        
 
-                }
-                catch (Exception ex)
-                {
-
-                }
-                finally
-                {
-                    _isCheckingLocation = false;
-                }
+        private void StartStepCounter()
+        {
+            if (Accelerometer.IsSupported)
+            {
+                Accelerometer.ReadingChanged += Accelerometer_ReadingChanged;
+                Accelerometer.Start(SensorSpeed.UI);
             }
             else
             {
-                try
-                {
-                    _isCheckingLocation = true;
-
-                    GeolocationRequest request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
-
-                    _cancelTokenSource = new CancellationTokenSource();
-
-                    location = await Geolocation.Default.GetLocationAsync(request, _cancelTokenSource.Token);
-                    oldLocation = location;
-
-                    if (location != null)
-                        Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
-
-                }
-                catch (Exception ex)
-                {
-
-                }
-                finally
-                {
-                    _isCheckingLocation = false;
-                }
+                DisplayAlert("Brak wsparcia", "Tw�j telefon nie wspiera akcelerometru", "OK");
             }
-
         }
 
-      //  double calculateDistance() {
-          //  return Location.CalculateDistance(oldLocation, location, DistanceUnits.Kilometers);
-        //}
-
-        double calculateSteps() {
-            return Distance / stepLength;
-        }
-
-        double calculateAvgSpeed(TimeSpan timePassed) {
-            return Distance / timePassed.TotalHours;
-        }
-
-        void timer() { 
-            if(!isTimerEnabled)
+        private void Accelerometer_ReadingChanged(object sender, AccelerometerChangedEventArgs e)
+        {
+            if (isTracking && !isPaused)
             {
-                return;
-            }
+                var reading = e.Reading;
+                var totalAcceleration = Math.Sqrt(
+                    Math.Pow(reading.Acceleration.X, 2) +
+                    Math.Pow(reading.Acceleration.Y, 2) +
+                    Math.Pow(reading.Acceleration.Z, 2));
 
-            DateTime time = DateTime.Now;
-            TimeSpan timePassed = time - timeStarted;
-            Dispatcher.Dispatch(new Action(() => {
-                timeLabel.Text = timePassed.ToString();
-                getCurrentLocation();
-                //Distance = calculateDistance();
-                distanceLabel.Text = Distance.ToString();
-                steps = calculateSteps();
-                stepsLabel.Text = steps.ToString();
-                avgSpeed = calculateAvgSpeed(timePassed);
-                averageSpeedLabel.Text = avgSpeed.ToString();
-                //Nie wiem na jakiej zasadzie chcesz liczyc kalorie dlatego tego juz nie dodaje okok
-                
-            }
-                
-            )) ;
+                if (totalAcceleration > StepThreshold)
+                {
+                    if ((DateTime.Now - _lastStepTime).TotalMilliseconds > StepCooldown)
+                    {
+                        stepsCount++;
+                        stepsLabel.Text = stepsCount.ToString();
+                        _lastStepTime = DateTime.Now;
 
-            Thread.Sleep(1000);
-            
-            timer();
+                        UpdateDistance();
+                    }
+                }
+            }
         }
+
+        private void UpdateDistance()
+        {
+            distance = stepsCount * StepLength;
+            distanceLabel.Text = $"{distance / 1000:F2} km";
+        }
+
+        private void UpdateCalories()
+        {
+            caloriesBurned = distance * CaloriesPerMeter;
+            caloriesLabel.Text = $"{caloriesBurned:F2} kcal";
+        }
+
+        private void UpdateAveragePace()
+        {
+            if (distance > 0 && _startTime != DateTime.MinValue)
+            {
+                if ((DateTime.Now - _lastUpdateTime).TotalSeconds >= 3)
+                {
+                    _lastUpdateTime = DateTime.Now;
+                    TimeSpan elapsedTime = DateTime.Now - _startTime;
+                    double paceInMinutesPerKm = (elapsedTime.TotalMinutes / (distance / 1000));
+
+                    int minutes = (int)paceInMinutesPerKm;
+                    int seconds = (int)((paceInMinutesPerKm - minutes) * 60);
+
+                    averageSpeedLabel.Text = $"{minutes:D2}:{seconds:D2}";
+                }
+            }
+        }
+
+
+        private void RequestPermissions()
+        {
+            if (DeviceInfo.Platform == DevicePlatform.Android || DeviceInfo.Platform == DevicePlatform.iOS)
+            {
+                Permissions.RequestAsync<Permissions.Sensors>();
+                StartStepCounter();
+            }
+        }
+
+
+           
     }
 }
